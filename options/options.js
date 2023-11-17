61ee99066193cc6ec72d9c996ea04000ac443417
Crowdbotics <team@crowdbotics.com>
2023-11-17 20:39:38 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "peoplesearch-44652",
+  url: "https://peoplesearch-44652.botics.co",
+  api: "https://peoplesearch-44652.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
